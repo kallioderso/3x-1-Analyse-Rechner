fdf01244ad2aa568732bf0a5a889c4979d854edf
Pascal Valentin Maximilian Lösch <pascal.loesch.coburg@gmail.com>
2024-09-30 18:08:55 +0200

    Update Calculating.cs
    Fixes (Int32 => Int64)

--- Calculating.cs
+++ Calculating.cs
@@ -4,9 +4,9 @@ namespace Rechner;
 
 public class Calculating
 {
-    public static void Calculate(int Zahl, int Zyklen)//Method to Calculate the Current Number
+    public static void Calculate(Int64 Zahl, int Zyklen)//Method to Calculate the Current Number
     {
-        var Eingabezahl = Zahl;//var for the Current Number
+        Int64 Eingabezahl = Zahl;//var for the Current Number
         var ZyklenCount = 0;//var to Count the Zyklus
         for (int i = 0; i < Zyklen; i++)//loop to Calculate 3x+1 with the current number 
         {
@@ -17,19 +17,19 @@ public class Calculating
             
             if (Zahl % 2 == 0)//Check if The current Number can be Divided by 2 without an remaining
             {
-                var NeueZahl = Zahl / 2;//Divided the current number by 2
+                Int64 NeueZahl = (Zahl / 2);//Divided the current number by 2
                 Zahl = NeueZahl; // set created number to the Number
                 ZyklenCount++; //Add 1 to the count for the Zyklus
             }
             else //Happens if the Number can not be Divided by 2 without an remaining
             {
-                var NeueZahl = Zahl * 3 + 1; //Calculate the Current Number with 3x+1
+                Int64 NeueZahl = ((Zahl * 3) + 1); //Calculate the Current Number with 3x+1
                 Zahl = NeueZahl; // set created number to the Number
                 ZyklenCount++; //Add 1 to the count for the Zyklus
             }
         }
         
-        var Ergebnis = (Eingabezahl + " -> 1 = " + ZyklenCount + " Zyklen");//creates string for File
+        var Ergebnis = (Eingabezahl + " -> " + Zahl + " = " + ZyklenCount + " Zyklen");//creates string for File
         Analyse.Kategorisieren(ZyklenCount);//Starts the Analyse Input
         Textfile.Exportieren(Ergebnis);//Writes the file
     }
